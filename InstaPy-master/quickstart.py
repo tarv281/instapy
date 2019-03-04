1a48e0c8e35ac2d4aac659299d20a37b00ca3ca4
tarv281 <39852294+tarv281@users.noreply.github.com>
2019-03-04 08:10:32 +0530

    Add files via upload

---
+++ InstaPy-master/quickstart.py
@@ -0,0 +1,21 @@
+""" Quickstart script for InstaPy usage """
+
+# imports
+from instapy import InstaPy
+from instapy import smart_run
+from instapy import set_workspace
+
+
+# set workspace folder at desired location (default is at your home folder)
+set_workspace(path=None)
+
+# get an InstaPy session!
+session = InstaPy()
+
+with smart_run(session):
+    """ Activity flow """
+    # general settings
+    session.set_dont_include(["friend1", "friend2", "friend3"])
+
+    # activity
+    session.like_by_tags(["natgeo"], amount=10)
